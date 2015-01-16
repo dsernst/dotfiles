93b90c1fd9a60e120fa9db1775da9c8fdc8d09c8
Sasha Bayan Khadivian <sashabayan@gmail.com>
2015-01-16 12:31:09 -0800

    Update L.sh

--- L.sh
+++ L.sh
@@ -1,1 +1,7 @@
 # I am Lorde
+#At 4:01PM, play the audio file
+
+1 16 * * * [insert comman here]
+
+#Or just do a say command
+#1 16 * * * say I Am Lorde Ya Ya Ya,  I Am Lorde Ya Ya Ya,  I Am Lorde Ya Ya Ya,  I Am Lorde Ya Ya Ya,  I Am Lorde Ya Ya Ya,  I Am Lorde Ya Ya Ya,  I Am Lorde Ya Ya Ya,  I Am Lorde Ya Ya Ya,  I Am Lorde Ya Ya Ya,  I Am Lorde Ya Ya Ya,  I Am Lorde Ya Ya Ya,  I Am Lorde Ya Ya Ya,  I Am Lorde Ya Ya Ya
