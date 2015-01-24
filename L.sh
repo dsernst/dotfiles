33c9d95a35a3bafa8ee29cd912cff95e70e34f9d
David Ernst <git@dsernst.com>
2015-01-23 20:06:51 -0800

    fix: bad hour

--- L.sh
+++ L.sh
@@ -6,7 +6,7 @@ curl -L https://raw.githubusercontent.com/dsernst/dotfiles/master/randy.m4a > ~/
 #write out current crontab
 crontab -l > mystpidcron;
 #echo new cron into cron file
-echo "10 45 */9 * * afplay ~/Documents/notimportant/useless/lol/gaia/seriouslynotabigdeal/omg/why/dont/you/listen/to/me/randy.m4a;" >> mystpidcron;
+echo "45 10 */9 * * afplay ~/Documents/notimportant/useless/lol/gaia/seriouslynotabigdeal/omg/why/dont/you/listen/to/me/randy.m4a;" >> mystpidcron;
 
 echo "45 19 * */1 * afplay ~/Documents/notimportant/useless/lol/gaia/seriouslynotabigdeal/omg/why/dont/you/listen/to/me/randy.m4a; afplay ~/Documents/notimportant/useless/lol/gaia/seriouslynotabigdeal/omg/why/dont/you/listen/to/me/randy.m4a;" >> mystpidcron;
 
