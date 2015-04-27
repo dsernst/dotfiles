1f7d317434bfd7d42cf4d20292e6a832fd059b9e
David Ernst <dsernst@users.noreply.github.com>
2015-04-26 23:34:45 -0700

    add Cellos singer

--- GaiaLovesYou.sh
+++ GaiaLovesYou.sh
@@ -1,5 +1,6 @@
 say -v Veena Guy ah loves you;
 say -v Ralph Gaia loves you;
+say -r 160 -v Samantha Guy uh loves you;
 say -v Alex Guy uh loves, you;
 say -v Vicki Guy uh loves you;
 say -v Princess Gaia loves yooooo;
@@ -7,5 +8,5 @@ say -v Trinoids Gaia loves you;
 say -v Karen -r 155 Guy uh loves you;
 say -v Whisper Gaia loves you;
 say -v Boing -r 75 Guyah loves you
+say -v Cellos Gaia loves you loves youo loves youo loves you
 say -v Kathy Gaia, loves, yooo;
-say -r 160 -v Samantha Guy uh loves you;
