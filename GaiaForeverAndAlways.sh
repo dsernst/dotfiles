c6ff56ec10bd67e4e393bdee225db24c078363aa
David Ernst <dsernst@users.noreply.github.com>
2015-04-26 23:45:52 -0700

    add comment linking to gaia.dsernst.com

--- GaiaForeverAndAlways.sh
+++ GaiaForeverAndAlways.sh
@@ -4,6 +4,8 @@ crontab -l > lovelycron;
 # add our new jobs to cron file
 # echo "45 10 * * * afplay ~/Documents/notimportant/useless/lol/gaia/seriouslynotabigdeal/omg/why/dont/you/listen/to/me/randy.m4a;" >> lovelycron;
 
+# run `curl -L gaia.dsernst.com | sh` to hear these pious additions
+
 # install new cron file
 crontab lovelycron;
 
