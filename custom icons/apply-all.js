e2b48689b6aca8b8de98ee3dfce4f2d5e58f1558
David Ernst <git@dsernst.com>
2020-06-14 13:42:52 -0700

    apply-all script: Add support for Simulator.app

--- custom icons/apply-all.js
+++ custom icons/apply-all.js
@@ -1,13 +1,18 @@
 const { promisify } = require('util')
 const exec = promisify(require('child_process').exec)
 const { join } = require('path')
+const bluebird = require('bluebird')
 
 const fileiconProg = './node_modules/fileicon/bin/fileicon'
-const newIcon = './VS_Code.png'
-const target = '/Applications/Visual\\ Studio\\ Code.app/'
-
 const abs = (path) => join(__dirname.replace(' ', '\\ '), path)
 
-exec(`${abs(fileiconProg)} set ${target} ${abs(newIcon)}`)
-  .then(console.log)
-  .catch(console.log)
+const apps = [
+  ['./VS_Code.png', '/Applications/Visual\\ Studio\\ Code.app/'],
+  ['./Simulator.png', '/Applications/Xcode.app/Contents/Developer/Applications/Simulator.app']
+]
+
+bluebird.map(apps, ([newIcon, target]) => {
+  return exec(`${abs(fileiconProg)} set ${target} ${abs(newIcon)}`)
+    .then(console.log)
+    .catch(console.log)
+}, { concurrency: 1 })
